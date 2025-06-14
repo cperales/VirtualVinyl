64b1166c876b5ea7c247effb82ff1ed9c3cdd75b
cperales <sir.perales@gmail.com>
2025-06-14 21:00:28 +0200

    New branch

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import './assets/main.css'
+import './style.css'
+
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import LoginView from './views/LoginView.vue'
+import HomeView from './views/HomeView.vue'
+
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: LoginView },
+  { path: '/home', component: HomeView },
+  { path: '/callback', redirect: '/home' } // Add callback route that redirects to home
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+createApp(App).use(router).mount('#app')
